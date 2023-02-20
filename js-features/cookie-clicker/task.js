4ec0500bd7b5977be8e3d35a81500d7d18d04888
Anastasia Uskova <uskova0aa@gmail.com>
2023-02-20 20:17:13 +0500

    all done

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,27 @@
-//Task 1
+//Task 1, 2
 const cookie = document.getElementById('cookie');
 const clickerCounter = document.getElementById('clicker__counter');
 const clickerSpeed = document.getElementById('clicker__speed');
 
+let clickEnd, clickStart;
+
 cookie.onclick = function() {
   cookie.width = 300;
+ 
+  clickEnd = Date.now();
   clickerCounter.textContent++;
+  if(clickerCounter.textContent == 1) {
+    clickerSpeed.textContent = '0.00';
+    clickStart = clickEnd;
+  } 
+  else {
+    clickerSpeed.textContent = (1000/(clickEnd - clickStart)).toFixed(2);
+    clickStart = clickEnd;
+  }
 }
+
 
 setInterval(() => {
   cookie.width = 200;
-}, 300)
+}, 300);
 
