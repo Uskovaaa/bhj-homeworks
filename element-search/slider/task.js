b7c7b0a4c32255e5a176fb797e63ef083cd1477b
Anastasia Uskova <uskova0aa@gmail.com>
2023-03-22 09:29:11 +0500

    changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,49 +1,60 @@
-const sliderPrev = document.querySelector( '.slider__arrow_prev' );
-const sliderNext = document.querySelector( '.slider__arrow_next' );
+const sliderPrev = document.querySelector('.slider__arrow_prev');
+const sliderNext = document.querySelector('.slider__arrow_next');
 
-const sliderItem = document.querySelectorAll( '.slider__item' );
-const sliderDot = document.querySelectorAll( '.slider__dot' );
+const sliderItem = document.querySelectorAll('.slider__item');
+const sliderDot = document.querySelectorAll('.slider__dot');
 
 
+let sliderItemArray = Array.from(sliderItem); //слайдер с фотографиями преобразован в массив
+
+//поиск позиции активного слайда
+let index = sliderItemArray.findIndex(value => {         //индекс элемента сохраняется в переменную index
+  if(value.classList.contains('slider__item_active')) { //если слайд активен, то возвращаем сам элемент
+    return value;                                       //метод findIndex возвращает индекс данного элемента
+  }
+});
+
+//функция деактивации слайда и точки на позиции index
+function remover(index) {
+  sliderItem[index].classList.remove('slider__item_active');
+  sliderDot[index].classList.remove('slider__dot_active');
+}
+
+//функция проверки и добавляения новых слайда и индекса 
+function check(position) { 
+  if(position < 0) {                 
+    position = sliderItem.length - 1; 
+    index = sliderItem.length - 1;    //необходимо обновить значение переменной index, иначе позиция будет меньше 0
+  }
+
+  if(position >= sliderItem.length) {
+    position = 0;
+    index = 0; //необходимо обновить значение переменной index, иначе позиция будет больше длины массива
+  }
+  sliderItem[position].classList.add('slider__item_active');
+  sliderDot[position].classList.add('slider__dot_active');
+}
+
+//событие клика на правую стрелку (вперёд)
 sliderNext.onclick = () => {
-  for( let i = 0; i < sliderItem.length; i++ ) {
-    if( sliderItem[ i ].classList.contains( 'slider__item_active' ) && sliderDot[ i ].classList.contains( 'slider__dot_active' )) {
-      sliderItem[ i ].classList.remove( 'slider__item_active' );
-      sliderDot[ i ].classList.remove( 'slider__dot_active' );
-      if( i == 4 ) {
-        i = - 1;
-      }
-      i++;
-      sliderItem[ i ].classList.add( 'slider__item_active' );
-      sliderDot[ i ].classList.add( 'slider__dot_active' );
-    }
-  }
+  remover(index); //деактивируем активный слайд
+  index++;        
+  check(index);   //передаём в функцию значение слайда +1
 }
 
+//событие клика на левую стрелку (назад)
 sliderPrev.onclick = () => {
-  for( let i = 0; i < sliderItem.length; i++ ) {
-    if( sliderItem[ i ].classList.contains( 'slider__item_active' ) && sliderDot[ i ].classList.contains( 'slider__dot_active' )) {
-      sliderItem[ i ].classList.remove( 'slider__item_active' );
-      sliderDot[ i ].classList.remove( 'slider__dot_active' );
-      if( i == 0 ) {
-        i = 5;
-      }
-      i--;
-      sliderItem[ i ].classList.add( 'slider__item_active' );
-      sliderDot[ i ].classList.add( 'slider__dot_active' );
-    }
-  }
+  remover(index); //деактивируем активный слайд
+  index--;
+  check(index);   //передаём в функцию значение слайда -1
 }
 
-
-  for( let i = 0; i < sliderDot.length; i++ ) {
-    if( sliderDot[i].classList.contains( 'slider__dot_active' ) ) a = i;
-      sliderDot[ i ].onclick = () => {
-
-        sliderDot[ a ].classList.remove( 'slider__dot_active' );
-        sliderItem[ a ].classList.remove( 'slider__item_active' );
-        a = i;
-        sliderDot[ i ].classList.add( 'slider__dot_active' );
-        sliderItem[ i ].classList.add( 'slider__item_active' );
-    }
+//собыие клика на точку
+ for( let i = 0; i < sliderDot.length; i++ ) {
+  sliderDot[i].onclick= () => { //получаем позицию точки, на которую кликнули
+   let k = index;               //запоминаем позицию активного элемента
+    index = i;                  //запоминаем позицию точки, на которую кликнули
+    check(index);               //передаём позицию точки, на которую кликнули, в функцию проверки для отображения
+    remover(k);                 //передаём позицию активного слайда в функцию деактиации
   }
+ }
