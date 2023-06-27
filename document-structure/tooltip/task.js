28b7d3635410f2a3a1c1f67292bf9af6908e4bc6
Anastasia Uskova <uskova0aa@gmail.com>
2023-06-27 15:11:00 +0500

    1st task completed

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,32 @@
+const tooltip = document.querySelector('.tooltip');
+const hasTooltip = document.querySelectorAll('.has-tooltip');
+
+tooltip.dataset.position = 'bottom';
+
+hasTooltip.forEach(e => {
+  e.onclick = () => {
+    console.log(e.getBoundingClientRect().left, e.getBoundingClientRect().width);
+    if(tooltip.dataset.position) {
+      if(tooltip.dataset.position == 'top') {
+        tooltip.style.top = e.getBoundingClientRect().top - 1.5 * e.getBoundingClientRect().height + 'px';
+      }
+      if(tooltip.dataset.position == 'right') {
+        tooltip.style.top = e.getBoundingClientRect().top + 'px';
+        tooltip.style.left = e.getBoundingClientRect().right + 'px';
+      }
+      if(tooltip.dataset.position == 'bottom') {
+        tooltip.style.top = e.getBoundingClientRect().bottom + 'px';
+        tooltip.style.left = e.getBoundingClientRect().left + 'px';
+      }
+    }
+    
+    tooltip.classList.add('tooltip_active');
+    tooltip.innerHTML = e.title;
+    return false;
+  }
+  
+});
+
+
+
+
